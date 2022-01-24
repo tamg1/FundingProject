2e35d08461d1f5ff94099c7d3c9c138ff41baf39
Felix Grund <felix@livelyapps.com>
2022-01-24 10:18:19 -0800

    A2 solution

--- src/test/model/ProjectInNeedTest.java
+++ src/test/model/ProjectInNeedTest.java
@@ -1,5 +1,49 @@
 package model;
 
-class ProjectInNeedTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ProjectInNeedTest {
+    private ProjectInNeed project;
+
+    @BeforeEach
+    void runBefore() {
+        project = new ProjectInNeed("Community Food Bank", 10000);
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("Community Food Bank", project.getTitle());
+        assertEquals(10000, project.getTarget());
+        assertEquals(0, project.getAmountFunded());
+    }
+
+    @Test
+    void testIncreaseTargetBy() {
+        project.increaseTargetBy(100);
+        assertEquals(10100, project.getTarget());
+
+        project.increaseTargetBy(200);
+        assertEquals(10300, project.getTarget());
+    }
+
+    @Test
+    void testDecreaseTargetBy() {
+        project.decreaseTargetBy(100);
+        assertEquals(9900, project.getTarget());
+
+        project.decreaseTargetBy(200);
+        assertEquals(9700, project.getTarget());
+    }
+
+    @Test
+    void testFundProject() {
+        project.fund(500);
+        assertEquals(500, project.getAmountFunded());
+
+        project.fund(750);
+        assertEquals(1250, project.getAmountFunded());
+    }
 }
--- src/test/model/SponsorTest.java
+++ src/test/model/SponsorTest.java
@@ -1,5 +1,30 @@
 package model;
 
-class SponsorTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SponsorTest {
+    private Sponsor testSponsor;
+
+    @BeforeEach
+    void runBefore() {
+        testSponsor = new Sponsor("Mary Mae");
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("Mary Mae", testSponsor.getName());
+        assertEquals(0, testSponsor.getNumProjectsFunded());
+    }
+
+    @Test
+    void testFundProject() {
+        testSponsor.fundProject();
+        assertEquals(1, testSponsor.getNumProjectsFunded());
+        testSponsor.fundProject();
+        assertEquals(2, testSponsor.getNumProjectsFunded());
+    }
 }
